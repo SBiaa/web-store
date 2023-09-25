48cd6bde0242cb37f228575a501a3e9a8af3e9fc
Bia Siqueira <developer.biaa@gmail.com>
2023-09-25 01:57:25 -0300

    add crud de marca e categoria

--- backend/src/main/java/com/dev/backend/controller/CategoriaController.java
+++ backend/src/main/java/com/dev/backend/controller/CategoriaController.java
@@ -6,43 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.backend.entity.Estado;
-import com.dev.backend.service.EstadoService;
+import com.dev.backend.entity.Categoria;
+import com.dev.backend.service.CategoriaService;
 
-import jakarta.websocket.server.PathParam;
+
+
 
 @RestController
-@RequestMapping("/api/estado")
-public class EstadoController {
-  
+@RequestMapping("/api/categoria")
+public class CategoriaController {
+
   @Autowired
-  private EstadoService estadoService;
+  private CategoriaService categoriaService;
 
   @GetMapping("/")
-  public List<Estado> buscarTodos(){
-    return estadoService.buscarTodos();
+  public List<Categoria> buscarTodas(){
+    return categoriaService.buscarTodas();
   }
 
   @PostMapping("/")
-  public Estado inserir(@RequestBody Estado estado){
-    return estadoService.inserir(estado);
+  public Categoria inserir(@RequestBody Categoria categoria ){
+    return categoriaService.inserir(categoria);
   }
 
   @PutMapping("/")
-  public Estado alterar(@RequestBody Estado estado){
-    return estadoService.alterar(estado);
+  public Categoria alterar(@RequestBody  Categoria categoria){
+    return categoriaService.alterar(categoria);
   }
-
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> excluir(@PathParam("id") Long id){
-    estadoService.excluir(id);
+  public ResponseEntity<Void> excluir(@PathVariable("id")Long id){
+    categoriaService.excluir(id);
     return ResponseEntity.ok().build();
   }
+  
+
 }
--- backend/src/main/java/com/dev/backend/controller/MarcaController.java
+++ backend/src/main/java/com/dev/backend/controller/MarcaController.java
@@ -6,43 +6,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.backend.entity.Estado;
-import com.dev.backend.service.EstadoService;
+import com.dev.backend.entity.Marca;
+import com.dev.backend.service.MarcaService;
 
-import jakarta.websocket.server.PathParam;
 
 @RestController
-@RequestMapping("/api/estado")
-public class EstadoController {
-  
+@RequestMapping("/api/marca")
+public class MarcaController {
+
   @Autowired
-  private EstadoService estadoService;
+  private MarcaService marcaService;
 
   @GetMapping("/")
-  public List<Estado> buscarTodos(){
-    return estadoService.buscarTodos();
+  public List<Marca> buscarTodas(){
+    return marcaService.buscarTodas();
   }
 
   @PostMapping("/")
-  public Estado inserir(@RequestBody Estado estado){
-    return estadoService.inserir(estado);
+  public Marca inserir(@RequestBody Marca marca ){
+    return marcaService.inserir(marca);
   }
 
   @PutMapping("/")
-  public Estado alterar(@RequestBody Estado estado){
-    return estadoService.alterar(estado);
+  public Marca alterar(@RequestBody  Marca marca){
+    return marcaService.alterar(marca);
   }
-
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> excluir(@PathParam("id") Long id){
-    estadoService.excluir(id);
+  public ResponseEntity<Void> excluir(@PathVariable("id")Long id){
+    marcaService.excluir(id);
     return ResponseEntity.ok().build();
   }
+  
+
 }
